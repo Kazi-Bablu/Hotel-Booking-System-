5d47f5843dcfad452530d4624a44c82f6c6e4593
Kazi-Bablu <kazibablubif@gmail.com>
2017-11-15 07:43:05 +0600

    laravel full project upload

---
+++ resources/views/contents/upload.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+
+        <div class="medium-6 large-5 columns">
+        <form>
+          {{csrf_field()}}
+          <form method="post" enctype="multipart/form-data">
+            <input type="file" name="image_upload" />
+            <small class="error">{{$errors->first('image_upload')}}</small>
+            <input type="submit" value="UPLOAD" class="button success hollow" />
+          
+        </form>
+      </div>
+    </div>
+@endsection
